1f55b7a2bec5bb43539aa1bd0d512ca3908bbe41
David-Jhon <106485025+David-Jhon@users.noreply.github.com>
2024-06-04 03:04:01 +0600

    Update log.js

--- log.js
+++ log.js
@@ -1,37 +1,35 @@
 const axios = require('axios');
 
+const adminGroupChatId = process.env.GC_ID;
+const botToken = process.env.BOT_TOKEN;
+
 let loggingEnabled = true;
 
-async function logMessage(msg, bot) {
+async function logMessage(msg) {
   const chatId = msg.chat.id || '';
   const senderName = msg.from.first_name + (msg.from.last_name ? ` ${msg.from.last_name}` : '');
   let logEntry = `🤖『 Bot Logs 』\n\n👤 | ${senderName}\n🪪 | @${msg.from.username}\nID  | ${msg.from.id}\n💬 | ${chatId}\n\nMessage:\n» `;
 
   if (msg.text) {
     logEntry += msg.text;
-  } else if (msg.photo) {
-    const highestResPhoto = msg.photo[msg.photo.length - 1];
-    logEntry += `Photo [File ID: ${highestResPhoto.file_id}]`;
-  } else if (msg.document) {
-    logEntry += `Document [File ID: ${msg.document.file_id}, Name: ${msg.document.file_name}]`;
-  } else if (msg.sticker) {
-    logEntry += `Sticker [File ID: ${msg.sticker.file_id}]`;
-  } else if (msg.video) {
-    logEntry += `Video [File ID: ${msg.video.file_id}]`;
+    console.log(logEntry);
+
+    // Only send log to admin chat if logging is enabled
+    if (loggingEnabled) {
+      try {
+        await sendTelegramMessage(botToken, adminGroupChatId, logEntry);
+      } catch (error) {
+        console.error('Failed to send log message to admin group chat:', error);
+      }
+    }
   } else {
-    logEntry += 'some other type of message';
-  }
-
-  console.log(logEntry);
-
-  // Only send log to admin chat if logging is enabled
-  if (loggingEnabled) {
-    const adminGroupChatId = process.env.GC_ID;
-    const botToken = process.env.BOT_TOKEN;
-    try {
-      await sendTelegramMessage(botToken, adminGroupChatId, logEntry);
-    } catch (error) {
-      console.error('Failed to send log message to admin group chat:', error);
+    // Forward non-text messages to the admin group chat if logging is enabled
+    if (loggingEnabled) {
+      try {
+        await forwardTelegramMessage(botToken, adminGroupChatId, msg.chat.id, msg.message_id);
+      } catch (error) {
+        console.error('Failed to forward message to admin group chat:', error);
+      }
     }
   }
 }
@@ -45,6 +43,19 @@ async function sendTelegramMessage(botToken, chatId, text) {
     });
   } catch (error) {
     console.error('Failed to send log message via Telegram API:', error);
+  }
+}
+
+async function forwardTelegramMessage(botToken, chatId, fromChatId, messageId) {
+  const url = `https://api.telegram.org/bot${botToken}/forwardMessage`;
+  try {
+    await axios.post(url, {
+      chat_id: chatId,
+      from_chat_id: fromChatId,
+      message_id: messageId
+    });
+  } catch (error) {
+    console.error('Failed to forward message via Telegram API:', error);
   }
 }
 
