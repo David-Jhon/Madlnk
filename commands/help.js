6e1f1ce8a7cef0bcd64a4d7881e43584e1d13dad
David-Jhon <106485025+David-Jhon@users.noreply.github.com>
2024-09-13 17:48:08 +0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,25 +14,31 @@ module.exports = function (bot) {
 • */start* - Get started with Bot
 • */help* - Show this help message
 
-*Channels*
+*Anime & Manga Channels*
 • */anime* - Get anime channel
 • */manga* - Get manga channel
 
-*Information*
-• */animeinfo* - Get information about an anime
-• */mangainfo* - Get information about a manga
+*Anime & Manga Information*
+• */animeinfo* - Get detailed information about an anime
+• */mangainfo* - Get detailed information about a manga
 • */anilist* - View your AniList activity and stats
-
-*Utilities*
-• */request* - Request anime/manga or give suggestions
-• */list* - Get the list of Archive
 • */lastairing* - Recent airing anime episodes
 • */top* - Retrieve top anime or manga titles
-• */browse* - Get info about popular, trending or upcoming animes
+• */browse* - Get info about popular, trending, or upcoming anime
 • */random* - Get a random anime and start watching it
 • */fillers* - Search for anime fillers
+
+*Download*  
+• */nhentai* - Get detailed info and read doujin
+
+*Requests & Suggestions*
+• */request* - Request anime/manga or give suggestions
+
+*Other Utilities*
+• */list* - Get the list of the Archive
 • */upload* - Upload a photo, GIF, or video to Imgur`;
-        bot.sendMessage(message.chat.id, helpResponse, {parse_mode: 'Markdown'});
+
+        bot.sendMessage(message.chat.id, helpResponse, { parse_mode: 'Markdown' });
       });
     }
   });
