26b39f5de1aff4c8591ae1a7eb96efe266e4637d
David-Jhon <106485025+David-Jhon@users.noreply.github.com>
2025-08-03 13:43:59 +0600

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,13 +1,40 @@
-module.exports = {
-    name: 'start',
-    version: 1.0,
-    longDescription: '',
-    shortDescription: 'Get started with Bot',
-    guide: '{pn}',
-    category: ['Getting Started', 1],
+// commands/start.js
+const AnimeCommand = require('./anime.js');
 
-    onStart: async ({ bot, msg, args }) => {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, `Hey there! \`${msg.from.first_name}\` 😃 \n\n𝗜'𝗺 𝗮 𝗠𝗔𝗗 𝗯𝗼𝘁. I can help you find anime, manga, manhwa, manhua. \nTo see the list of all commands, type /help.`, { parse_mode: 'Markdown'});
+module.exports = {
+  name: 'start',
+  version: 1.0,
+  longDescription: 'Start interacting with the bot.',
+  shortDescription: 'Get started with Bot',
+  guide: '{pn} [payload]',
+  category: ['Getting Started', 1],
+
+  onStart: async ({ bot, msg, args }) => {
+    const chatId = msg.chat.id;
+
+    if (args && args.length > 0) {
+      const payload = args[0].replace(/-/g, '+').replace(/_/g, '/');
+      try {
+        const decodedPayload = Buffer.from(payload, 'base64').toString('utf8');
+        const [type] = decodedPayload.split('-');
+
+        if (type === 'anime' && AnimeCommand.onStartDeepLink) {
+          await AnimeCommand.onStartDeepLink({ bot, msg, payload });
+          return;
+        }
+
+        await bot.sendMessage(chatId, 'Invalid action.', { parse_mode: 'Markdown' });
+      } catch (error) {
+        console.error('Error processing deep link:', error);
+        await bot.sendMessage(chatId, 'An error occurred while processing the deep link.', { parse_mode: 'Markdown' });
+      }
+      return;
     }
-  };
+
+    await bot.sendMessage(
+      chatId,
+      `Hey there! \`${msg.from.first_name}\` 😃 \n\n𝗜'𝗺 𝗮 𝗠𝗔𝗗 𝗯𝗼𝘁. I can help you find anime, manga, manhwa, manhua. \nTo see the list of all commands, type /help.`,
+      { parse_mode: 'Markdown' }
+    );
+  },
+};
